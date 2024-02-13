c0b2ab0fd7e0c1f0481595db7e74e8f8d4ab6e33
Breno Souza Costa <brenosouza.dev@gmail.com>
2024-02-13 16:52:30 -0300

    Completed design for sidebar ButtonMenu

--- src/components/ButtonMenu/styles.ts
+++ src/components/ButtonMenu/styles.ts
@@ -1,4 +1,14 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeInFromLeft = keyframes`
+  0%   {
+    transform: translateX(-5rem);
+  }
+
+  100% {
+    transform: translateX(0rem);
+    }
+`;
 
 export const ButtonMenu = styled.button`
   border: transparent;
@@ -7,7 +17,25 @@ export const ButtonMenu = styled.button`
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: relative;
   width: 100%;
+  transition: background-color 0.3s;
+  animation: ${fadeInFromLeft} 0.5s;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+  &:focus {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+  &::before {
+    content: "";
+    width: 0.2rem;
+    height: 100%;
+    background-color: #375bb6;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 `;
 
 export const Icon = styled.i``;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import { ButtonMenu } from "../ButtonMenu";
 import * as S from "./styles";
 
 const Sidebar = () => {
+  const [buttonActive, setButtonActive] = useState(0);
+  function handleBtActive(value: number) {
+    setButtonActive(value);
+  }
   return (
     <S.Sidebar>
-      <ButtonMenu text="Clique aquidsds" />
-      <ButtonMenu text="Clique aqui" /> <ButtonMenu text="Clique aqui" />{" "}
-      <ButtonMenu text="Clique aqui" /> <ButtonMenu text="Clique aqui" />{" "}
+      <ButtonMenu
+        text="Clique aquidsds"
+        onClick={() => {
+          handleBtActive(1);
+        }}
+      />
+      <ButtonMenu
+        text="Clique aqui"
+        onClick={() => {
+          handleBtActive(2);
+        }}
+      />{" "}
+      <ButtonMenu
+        text="Clique aqui"
+        onClick={() => {
+          handleBtActive(3);
+        }}
+      />{" "}
+      <ButtonMenu text="Clique aqui" /> <ButtonMenu text="Clique aqui" />
       <ButtonMenu text="Clique aqui" /> <ButtonMenu text="Clique aqui" />
     </S.Sidebar>
   );
